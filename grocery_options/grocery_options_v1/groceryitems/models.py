7ef8850e142346d3224b57632ae0c9ec35018ea8
Enoch Park <44038883+enochpark89@users.noreply.github.com>
2022-07-20 21:19:59 -0700

    20220720

--- grocery_options/grocery_options_v1/groceryitems/models.py
+++ grocery_options/grocery_options_v1/groceryitems/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+"""
+Columns
+- Date of offer: postdate
+- Supermarket company name: company_name
+- Price: price
+- Condition: deal
+- Brand: brand_name
+- Item name: item_name
+
+"""
+
+class Groceryitems(models.Model):
+  post_date = models.DateField()
+  company_name = models.CharField(max_length=30)
+  price = models.DecimalField(max_digits=10, decimal_places=2)
+  condition = models.CharField(max_length=30)
+  brand_name = models.CharField(max_length=30)
+  item_name = models.CharField(max_length=50)
--- grocery_options/grocery_options_v1/groceryitems/views.py
+++ grocery_options/grocery_options_v1/groceryitems/views.py
@@ -1,6 +1,35 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
+from .models import Groceryitems
+
 
 def index(request):
+  items = Groceryitems.objects.all().values()
   template = loader.get_template('index.html')
-  return HttpResponse(template.render())
+  # Create an object that contain data.
+  context = {
+    'items': items,
+  }
+  return HttpResponse(template.render(context, request))
+
+def add(request):
+  template = loader.get_template('add.html')
+  return HttpResponse(template.render({}, request))
+
+def addrecord(request):
+  post_date = request.POST['post_date']
+  company_name = request.POST['company_name']
+  price = request.POST['price']
+  condition = request.POST['condition']
+  brand_name = request.POST['brand_name']
+  item_name = request.POST['item_name']
+  item = Groceryitems(
+    post_date=post_date, 
+    company_name=company_name, 
+    price=price, 
+    condition=condition, 
+    brand_name=brand_name, 
+    item_name=item_name)
+  item.save()
+  return HttpResponseRedirect(reverse('index'))
